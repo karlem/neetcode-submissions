c0b3e9d4d4bd6c27b061678fbb782d645de60233
Karel Moravec <moravec.wdd@gmail.com>
2026-06-13 13:22:12 +0200

    Add: permutations - submission-0

---
+++ Data Structures & Algorithms/permutations/submission-0.go
@@ -0,0 +1,33 @@
+func permute(nums []int) [][]int {
+	var curr []int
+	var result [][]int
+	used := map[int]bool{}
+
+	// curr [1]
+	// used {1}
+
+	var dfs func()
+	dfs = func() {
+		if len(curr) == len(nums) {
+			cp := make([]int, len(curr))
+			copy(cp, curr)
+			result = append(result, cp)
+			return
+		}
+
+		for _, num := range nums {
+			if used[num] {
+				continue
+			}
+
+			curr = append(curr, num)
+			used[num] = true
+			dfs()
+			used[num] = false
+			curr = curr[:len(curr)-1]
+		}
+	}
+
+	dfs()
+	return result
+}
