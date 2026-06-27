993d60bced6a38d43bffd2471769dd75c5863994
Karel Moravec <moravec.wdd@gmail.com>
2026-06-27 11:41:36 +0200

    Add: one-edit-distance - submission-7

---
+++ Data Structures & Algorithms/one-edit-distance/submission-7.go
@@ -0,0 +1,23 @@
+func isOneEditDistance(s string, t string) bool {
+	m, n := len(s), len(t)
+
+	if m > n {
+		return isOneEditDistance(t, s)
+	}
+
+	if m-n > 1 {
+		return false
+	}
+
+	for i := 0; i < m; i++ {
+		if s[i] != t[i] {
+			if m == n {
+				return s[i+1:] == t[i+1:]
+			} else {
+				return s[i:] == t[i+1:]
+			}
+		}
+	}
+
+	return len(t) == len(s) + 1
+}
