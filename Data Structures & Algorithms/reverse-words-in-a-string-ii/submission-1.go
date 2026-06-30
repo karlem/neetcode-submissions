fd5efa32dd78c3ce08f19ef7561a1677531855b6
Karel Moravec <moravec.wdd@gmail.com>
2026-06-30 13:51:47 +0200

    Add: reverse-words-in-a-string-ii - submission-1

---
+++ Data Structures & Algorithms/reverse-words-in-a-string-ii/submission-1.go
@@ -0,0 +1,26 @@
+// ["t","h","e"," ","s","k","y"," ","i","s"," ","b","l","u","e"]
+// ["e","u","l","b"," ","s","i"," ","y","k","s"," ","e","h","t"]
+// ["b","l","u","e"," ","s","i"," ","y","k","s"," ","e","h","t"]
+
+func reverse(s []byte, left int, right int) {
+	for left < right {
+		s[left], s[right] = s[right], s[left]
+		left++
+		right--
+	}
+}
+
+func reverseWords(s []byte) {
+	reverse(s, 0, len(s)-1)
+
+	l := 0
+	for l < len(s) {
+		r := l
+		for r < len(s) && s[r] != ' ' {
+			r++
+		}
+
+		reverse(s, l, r-1)
+		l = r+1
+	}
+}
