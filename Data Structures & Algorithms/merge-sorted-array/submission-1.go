a72879f79bc7a18b0caaae316e345dc6e8c7b154
Karel Moravec <moravec.wdd@gmail.com>
2026-07-07 19:48:19 +0200

    Add: merge-sorted-array - submission-1

---
+++ Data Structures & Algorithms/merge-sorted-array/submission-1.go
@@ -0,0 +1,15 @@
+func merge(nums1 []int, m int, nums2 []int, n int) {
+	w := len(nums1) - 1
+	i, j := m - 1, n - 1
+
+	for j >= 0 {
+		if i >= 0 && nums1[i] > nums2[j] {
+			nums1[w] = nums1[i]
+			i--
+		} else {
+			nums1[w] = nums2[j]
+			j--
+		}
+		w--
+	}
+}
