01593f1b5c438b6e46182acb3b7855a6d27ce1b5
Karel Moravec <moravec.wdd@gmail.com>
2026-07-07 11:43:00 +0200

    Add: remove-element - submission-0

---
+++ Data Structures & Algorithms/remove-element/submission-0.go
@@ -0,0 +1,12 @@
+func removeElement(nums []int, val int) int {
+    w := 0
+
+	for i := 0; i < len(nums); i++ {
+		if nums[i] != val {
+			nums[w], nums[i] = nums[i], nums[w]
+			w++
+			continue
+		}
+	}
+	return w
+}
