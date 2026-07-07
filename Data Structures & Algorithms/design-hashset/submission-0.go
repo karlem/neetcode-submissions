c76da8bd77dd962c51cce9b153539b2561403cc6
Karel Moravec <moravec.wdd@gmail.com>
2026-07-07 13:56:09 +0200

    Add: design-hashset - submission-0

---
+++ Data Structures & Algorithms/design-hashset/submission-0.go
@@ -0,0 +1,30 @@
+type MyHashSet struct {
+	data []bool
+}
+
+func Constructor() MyHashSet {
+    return MyHashSet{
+		data: make([]bool, 100000),
+	}
+}
+
+func (this *MyHashSet) Add(key int) {
+    this.data[key] = true
+}
+
+func (this *MyHashSet) Remove(key int) {
+    this.data[key] = false
+}
+
+func (this *MyHashSet) Contains(key int) bool {
+    return this.data[key]
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.Add(key);
+ * obj.Remove(key);
+ * param_3 := obj.Contains(key);
+ */
+ 
