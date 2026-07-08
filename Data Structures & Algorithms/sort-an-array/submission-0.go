083a830f946fce08ee56cf188998d8825c4e0173
Karel Moravec <moravec.wdd@gmail.com>
2026-07-08 17:16:40 +0200

    Add: sort-an-array - submission-0

---
+++ Data Structures & Algorithms/sort-an-array/submission-0.go
@@ -0,0 +1,39 @@
+// [10,9,1,1,1,2,3,1]
+// [10,9,1,1] [1,2,3,1]
+// [10,9] [1,1] [1,2] [3,1]
+// [10] [9] [1] [1] [1] [2] [3] [1]
+// [9,10] [1,1] [1,2] [1,3]
+// [1,1,9,10] [1,1,2,3]
+// [1,1,1,1,2,3,9,10]
+
+// [5,10,2,1,3]
+// [5,10] [2,1,3]
+// [5] [10] [2] [1] [3]
+// [5, 10] [1,2,3]
+// [1,2,3,5,10]
+
+func sortArray(nums []int) []int {
+	if len(nums) == 1 {
+		return nums
+	}
+
+	half := len(nums)/2
+	left, right := sortArray(nums[:half]), sortArray(nums[half:])
+	i, j := 0,0
+	
+	res := []int{}
+	for i < len(left) && j < len(right) {
+		if left[i] < right[j] {
+			res = append(res, left[i])
+			i++
+		} else {
+			res = append(res, right[j])
+			j++
+		}
+	}
+
+	res = append(res, left[i:]...)
+	res = append(res, right[j:]...)
+
+	return res
+}
